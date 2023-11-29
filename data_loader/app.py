b6cea0f9fe1ea297abb9e92fa9db3284f654b857
sebastiancordoba <sebascordoba1@gmail.com>
2023-11-29 13:25:53 -0600

    Subiendo lo de cass

--- data_loader/app.py
+++ data_loader/app.py
@@ -79,110 +79,119 @@ for country_data in countries_data:
                     subregion_name=subregion_name, region_name=region_name)
 
 
+import requests
+import pandas as pd
+
+# Realizar la solicitud a la API
+url = 'https://restcountries.com/v3.1/all?fields=name,languages,gini,borders,timezones,unMember,capital,region,subregion,car,population,startOfWeek,capitalInfo,landlocked'
+response = requests.get(url)
+
+# Verificar si la solicitud fue exitosa (código de estado 200)
+if response.status_code == 200:
+    # Convertir la respuesta a formato JSON
+    data = response.json()
+
+    # Convertir JSON a un DataFrame de pandas
+    df = pd.json_normalize(data)
+
+    # Guardar el DataFrame como un archivo CSV
+    df.to_csv('countries_data.csv', index=False)
+
+    print("¡Datos guardados exitosamente como 'countries_data.csv'!")
+else:
+    print("Error al obtener los datos de la API.")
+
+
 from cassandra.cluster import Cluster
 
-# Connect to Cassandra
-cassandra_host = 'cassandra'
-cassandra_port = '9042'
-cassandra_keyspace = 'world'
-cassandra_table = 'countries'
+# Crear una conexión con el clúster de Cassandra
+cluster = Cluster()
+session = cluster.connect()
 
-# Connect to MongoDB
-mongo_uri = 'mongodb://mongo:27017/'
-mongo_database = 'world'
-mongo_collection = 'countries'
-
-# Create SparkSession
-spark = SparkSession.builder.appName('MongoDB to Cassandra ETL').getOrCreate()
-
-# Read data from MongoDB
-mongo_df = spark.read.format('mongo').option('uri', mongo_uri).option('database', mongo_database).option('collection', mongo_collection).load()
-
-# Transform data
-cassandra_df = mongo_df.select(
-    col('_id').cast('string').alias('id'),
-    col('startOfWeek'),
-    col('capitalInfo.latlng')[0].alias('capitalInfo_lat'),
-    col('capitalInfo.latlng')[1].alias('capitalInfo_lng'),
-    col('name.common').alias('name_common'),
-    col('name.official').alias('name_official'),
-    col('name.nativeName.eng.common').alias('name_nativeName'),
-    col('unMember'),
-    col('capital')[0].alias('capital').cast('string'),
-    col('region'),
-    col('subregion'),
-    col('languages.eng').alias('languages'),
-    col('landlocked'),
-    col('borders').cast('string'),
-    col('population'),
-    col('gini.2018').alias('gini'),
-    col('car.signs').cast('string').alias('car_signs'),
-    col('car.side').alias('car_side'),
-    col('timezones').cast('string')
-)
-
-# Write data to Cassandra
-cassandra_df.write.format('org.apache.spark.sql.cassandra').options(
-    **{
-        "table": cassandra_table,
-        "keyspace": cassandra_keyspace,
-        "spark.cassandra.connection.host": cassandra_host,
-        "spark.cassandra.connection.port": cassandra_port,
-    }
-).mode('append').save()
-
-
-# Write to alerta.txt
-try:
-    with open('alerta.txt', 'w') as f:
-        f.write('The code ran correctly.')
-except Exception as e:
-    with open('alerta.txt', 'w') as f:
-        f.write(f'The code did not run correctly. Error message: {str(e)}')
-from cassandra.auth import PlainTextAuthProvider
-# Connect to Cassandra
-auth_provider = PlainTextAuthProvider(username='cassandra', password='cassandra')
-cassandra_cluster = Cluster(['cassandra'], auth_provider=auth_provider)
-cassandra_session = cassandra_cluster.connect()
-cassandra_session.set_keyspace('world')
-
-# Prepare a Cassandra statement
-cassandra_stmt = cassandra_session.prepare("""
-    INSERT INTO countries (id, startOfWeek, capitalInfo_lat, capitalInfo_lng, name_common, name_official, name_nativeName, unMember, capital, region, subregion, languages, landlocked, borders, population, gini, car_signs, car_side, timezones)
-    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+# Crear un keyspace si no existe
+session.execute("""
+    CREATE KEYSPACE IF NOT EXISTS nombre_keyspace 
+    WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }
 """)
 
-# Read data from MongoDB and write it to Cassandra
-for doc in mongo_collection.find():
-    cassandra_session.execute(cassandra_stmt, (
-        str(doc['_id']),
-        doc['startOfWeek'],
-        doc['capitalInfo']['latlng'][0],
-        doc['capitalInfo']['latlng'][1],
-        doc['name']['common'],
-        doc['name']['official'],
-        doc['name']['nativeName']['eng']['common'],
-        doc['unMember'],
-        doc['capital'][0] if doc['capital'] else None,
-        doc['region'],
-        doc['subregion'],
-        doc['languages']['eng'],
-        doc['landlocked'],
-        ', '.join(doc['borders']),
-        doc['population'],
-        doc['gini']['2018'],
-        ', '.join(doc['car']['signs']),
-        doc['car']['side'],
-        ', '.join(doc['timezones'])
-    ))
+# Usar el keyspace
+session.set_keyspace('nombre_keyspace')
 
-# Write to alerta.txt
-try:
-    with open('alerta.txt', 'w') as f:
-        f.write('The code ran correctly.')
-except Exception as e:
-    with open('alerta.txt', 'w') as f:
-        f.write(f'The code did not run correctly. Error message: {str(e)}')
+# Cargar el archivo CSV en un DataFrame
+df_countries = pd.read_csv('countries_data.csv')
+
+# Columnas con las que trabajaremos
+relevant_columns = [
+    'startOfWeek', 'unMember', 'capital', 'region', 'subregion',
+    'landlocked', 'borders', 'population', 'timezones'
+]
+
+from ast import literal_eval
+
+# Lee el archivo CSV
+df = df_countries[relevant_columns]
+
+# Limpiar un poco los datos
+
+df['capital'] = df['capital'].apply(literal_eval)
+df['borders'] = df['borders'].apply(literal_eval)
+df['timezones'] = df['timezones'].apply(literal_eval)
+
+df['subregion'].fillna('', inplace=True)
+
+session.execute("""
+    CREATE TABLE IF NOT EXISTS countries (
+        startOfWeek text,
+        unMember boolean,
+        capital list<text>,
+        region text,
+        subregion text,
+        landlocked boolean,
+        borders list<text>,
+        population bigint,
+        timezones list<text>,
+        PRIMARY KEY (startOfWeek, region, population)
+    )
+""")
+
+# Suponiendo que 'df' es tu DataFrame y ya tienes la conexión establecida a tu cluster de Cassandra en 'session'
+
+for _, row in df.iterrows():
+    query = """
+        INSERT INTO countries (
+            startOfWeek, unMember, capital, region, subregion, landlocked, borders, population, timezones
+        ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    # Estructura explícitamente la tupla de inserción para coincidir con los marcadores de posición en la consulta
+    insert_values = (
+        row['startOfWeek'], row['unMember'], row['capital'], row['region'], row['subregion'],
+        row['landlocked'], row['borders'], row['population'], row['timezones']
+    )
+    
+    session.execute(query, insert_values)
+
+# Realiza una consulta para verificar que los datos se hayan insertado correctamente
+result = session.execute("SELECT * FROM countries LIMIT 5")
+for row in result:
+    print(row)
+
+# 1. Países en África con poblaciones superiores a 50 millones
+
+result = session.execute("SELECT * FROM countries WHERE region = 'Africa' AND population > 50000000 ALLOW FILTERING")
+for row in result:
+    print(row)
 
 
- 
+# 2. Países en Asia que tienen fronteras con más de cinco países
+
+result = session.execute("SELECT * FROM countries WHERE region = 'Asia' ALLOW FILTERING")
+filtered_rows = [row for row in result if row.borders and len(row.borders) > 5]
+for row in filtered_rows:
+    print(row)
+
+# 3. Países que tienen una capital con más de 6 letras y están en una región de África sin litoral
+
+result = session.execute("SELECT * FROM countries WHERE region = 'Africa' AND landlocked = True ALLOW FILTERING")
+filtered_rows = [row for row in result if len(row.capital[0]) > 6]
+for row in filtered_rows:
+    print(row)
